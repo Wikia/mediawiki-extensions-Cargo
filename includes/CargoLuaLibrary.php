5e1fd5ab753001c90f56063eb47ecba568855a9c
Łukasz Harasimowicz <dev@harnash.eu>
2025-04-11 12:04:01 +0200

    PLATFORM-11002| expose DB lua error to the user

--- includes/CargoLuaLibrary.php
+++ includes/CargoLuaLibrary.php
@@ -56,7 +56,7 @@ class CargoLuaLibrary extends LibraryBase {
 			$rows = $query->run();
 		// Fandom-start PLATFORM-9121 | Do not swallow DB errors in Cargo query
 		} catch ( \Wikimedia\Rdbms\DBError $e ) {
-			throw $e;
+			throw new Scribunto_LuaError( $e->getMessage() );
 		// Fandom-end
 		} catch ( Exception $e ) {
 			// Allow for error handling within Lua.
