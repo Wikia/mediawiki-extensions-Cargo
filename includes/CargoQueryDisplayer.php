90bc76bc269c9607cd9e308e51f4abc3934ebebf
Albert Skonieczny <askonieczny@fandom.com>
2025-09-29 14:18:14 +0200

    Revert "Merge pull request #46 from Wikia/REL1_43-Fandom-minor-upgrade"
    This reverts commit d224a8ca1e91c92f4608df69940385b6abcffcd7, reversing
    changes made to 4cdb081316def3b75384b451cf88d1711105e57e.

--- includes/CargoQueryDisplayer.php
+++ includes/CargoQueryDisplayer.php
@@ -237,10 +237,6 @@ class CargoQueryDisplayer {
 			// namespace; they are displayed as thumbnails within
 			// queries.
 			$title = Title::newFromText( $value, NS_FILE );
-			if ( !$title ) {
-				return null;
-			}
-
 			$file = MediaWikiServices::getInstance()->getRepoGroup()->findFile( $title );
 			return Linker::makeThumbLinkObj(
 				$title,
